7e92a7a8c37ad1abf8cabd7457b9c2d7dd2872af
Austin <30918019+haywang@users.noreply.github.com>
2024-11-03 21:08:17 +0800

    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,16 @@
-import Link from 'next/link'
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { MDXLayouterRender } from 'pliny/mdx-components'
+import { coreContent } from 'pliny/utils/contentlayer'
+import AuthorLayout from '@/layouts/AuthorLayout'
 
 export default function Page() {
+  const author = allAuthors.find((p) => p.slug === 'default') as Authors
+  const mainContent = coreContent(author)
   return (
-    <div>
-      <h1>About</h1>
-      <Link href="/">go Home</Link>
-    </div>
+    <>
+      <AuthorLayout content={mainContent}>
+        <MDXLayouterRender code={author.body.code} />
+      </AuthorLayout>
+    </>
   )
 }
