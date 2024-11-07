88db1597ab8ccd6cae6c1fe670a04e91b1875fd2
Austin <30918019+haywang@users.noreply.github.com>
2024-11-07 10:09:59 +0800

    add post route

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,3 +1,64 @@
+import { components } from '@/components/MDXComponents'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import {
+  sortPosts,
+  coreContent,
+  allCoreContent
+} from 'pliny/utils/contentlayer'
+import { allBlogs, allAuthors } from 'contentlayer/generated'
+import type { Authors, Blog } from 'contentlayer/generated'
+import PostSimple from '@/layouts/PostSimple'
+import siteMeta from '@/data/siteMeta'
+import { notFound } from 'next/navigation'
+
+const defaultLayout = 'PostSimple'
+const layouts = { PostSimple }
+
 export default function Page({ params }: { params: { slug: string[] } }) {
-  return <h1>This is a post page, params: {JSON.stringify(params)}</h1>
+  const slug = decodeURI(params.slug.join('/'))
+  const sortedCoreContents = allCoreContent(sortPosts(allBlogs))
+  const postIndex = sortedCoreContents.findIndex((p) => p.slug === slug)
+  if (postIndex === -1) {
+    return notFound()
+  }
+
+  const pre = sortedCoreContents[postIndex + 1]
+  const next = sortedCoreContents[postIndex - 1]
+  const post = allBlogs.find((p) => p.slug === slug) as Blog
+  const authorList = post?.authors || 'default'
+  const authorDetails = authorList.map((author) => {
+    const authorResults = allAuthors.find((p) => p.slug === author)
+    return coreContent(authorResults as Authors)
+  })
+  const mainContent = coreContent(post)
+  const jsonLd = post.structuredData
+  jsonLd['author'] = authorDetails.map((author) => {
+    return {
+      '@type': 'Person',
+      name: author.name
+    }
+  })
+
+  const Layout = layouts[post.layout || defaultLayout]
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      ></script>
+      <Layout
+        content={mainContent}
+        authorDetails={authorDetails}
+        next={next}
+        pre={pre}
+      >
+        <MDXLayoutRenderer
+          code={post.body.code}
+          components={components}
+          toc={post.toc}
+        ></MDXLayoutRenderer>
+      </Layout>
+    </>
+  )
 }
