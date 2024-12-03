f98ebbc341b92fe891bdcc05a036e627edd5db84
“Austin” <30918019+haywang@users.noreply.github.com>
2024-12-03 09:50:32 +0800

    done MobileNav component

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -9,20 +9,29 @@ import {
 import { useState, useEffect, useRef, Fragment } from 'react'
 
 import Menu from '@/data/menu.svg'
-// import { Transition } from '@headlessui/react'
+import {
+  Transition,
+  TransitionChild,
+  Dialog,
+  DialogPanel
+} from '@headlessui/react'
+import Close from '@/data/close.svg'
+import headerNavLinks from '@/data/headerNavLinks'
+import Link from './Link'
 
 const MobileNav = () => {
   const [navShow, setNavShow] = useState(false)
-  console.log(navShow)
+
   const navRef = useRef(null)
 
   const onToggleNav = () => {
+    console.warn('into toggle nav handle')
     setNavShow((status) => {
       if (status) {
+        console.warn('hidden')
         enableBodyScroll(navRef.current)
-        window.alert('hidden')
       } else {
-        window.alert('show')
+        console.warn('show')
         disableBodyScroll(navRef.current)
       }
       return !status
@@ -42,12 +51,57 @@ const MobileNav = () => {
       >
         <Menu className="h-8 w-8 text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400" />
       </button>
-      {/* <Transition
-        appear
-        show={navShow}
-        as={Fragment}
-        unmount={false}
-      ></Transition> */}
+      <Transition appear show={navShow} as={Fragment} unmount={false}>
+        <Dialog as="div" onClose={onToggleNav} unmount={false}>
+          <TransitionChild
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            unmount={false}
+          >
+            <div className="fix inset-0 z-60 bg-black/25"></div>
+          </TransitionChild>
+          <TransitionChild
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="translate-x-full opacity-0"
+            enterTo="translate-x-0 opacity-95"
+            leave="transition ease-in duration-200 transform"
+            leaveFrom="translate-x-0 opacity-95"
+            leaveTo="translate-x-full opacity-0"
+            unmount={false}
+          >
+            <DialogPanel className="fixed left-0 top-0 z-70 h-full w-full bg-white opacity-95 duration-300 dark:bg-gray-950 dark:opacity-[0.98]">
+              <nav
+                ref={navRef}
+                className="mt-8 flex h-full basis-0 flex-col items-start overflow-y-auto pl-12 pt-2 text-left"
+              >
+                {headerNavLinks.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className="mb-4 py-2 pr-4 text-2xl font-bold tracking-widest text-gray-900 outline outline-0 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                    onClick={onToggleNav}
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </nav>
+              <button
+                className="fixed right-4 top-7 z-80 h-16 w-16 p-4 text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                aria-label="Toggle Menu"
+                onClick={onToggleNav}
+              >
+                <Close />
+              </button>
+            </DialogPanel>
+          </TransitionChild>
+        </Dialog>
+      </Transition>
     </>
   )
 }
