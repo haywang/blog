c36f49a7785dc99761662775d5b2b88ec3455a49
“Austin” <30918019+haywang@users.noreply.github.com>
2024-12-05 16:03:40 +0800

    Generating a sitemap using code

---
+++ app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from 'next'
+import { allBlogs } from 'contentlayer/generated'
+import siteMeta from '@/data/siteMeta'
+import headerNavLinks from '@/data/headerNavLinks'
+
+export const dynamic = 'force-static'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const siteUrl = siteMeta.siteUrl
+
+  // filter publish post, and return url and lastMod
+  const blogRoutes = allBlogs
+    .filter((post) => !post.draft)
+    .map((post) => ({
+      url: `${siteUrl}/${post.path}`,
+      lastModified: post.lastmod || post.date
+    }))
+
+  const routes = headerNavLinks.map((route) => ({
+    url: `${siteUrl}${route.href}`,
+    lastModified: new Date().toISOString().split('T')[0]
+  }))
+
+  return [...routes, ...blogRoutes]
+}
