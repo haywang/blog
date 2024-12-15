9a73e0d6cf4462309c275e6355668417ce907790
“Austin” <30918019+haywang@users.noreply.github.com>
2024-12-15 20:58:38 +0800

    add one post about learning

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   // { href: '/tools', title: 'Tools' },
   // { href: '/learning', title: 'Learning' },
-  { href: 'https://portfolio.austinfe.com', title: 'Portfolio' },
+  // { href: 'https://portfolio.austinfe.com', title: 'Portfolio' },
   { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' }
 ]
