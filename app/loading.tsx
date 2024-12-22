0939098d2e44102b307aa11e37ef031dc1fbc1c0
“Austin” <30918019+haywang@users.noreply.github.com>
2024-12-22 22:20:06 +0800

    optimize loading bg

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
+    <div className="flex h-screen items-center justify-center bg-transparent">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-gray-200 border-t-blue-500 shadow-lg"></div>
     </div>
   )
