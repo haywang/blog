34f161f882ee14ebaf0c44500f4da04598187c52
“Austin” <30918019+haywang@users.noreply.github.com>
2025-01-19 19:39:25 +0800

    post page add metadata

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -8,11 +8,58 @@ import {
 import { allBlogs, allAuthors } from 'contentlayer/generated'
 import type { Authors, Blog } from 'contentlayer/generated'
 import PostSimple from '@/layouts/PostSimple'
-// import siteMeta from '@/data/siteMeta'
+import siteMeta from '@/data/siteMeta'
 import { notFound } from 'next/navigation'
+import { Metadata } from 'next'
 
 const defaultLayout = 'PostSimple'
 const layouts = { PostSimple }
+
+export async function generateMetadata(props: {
+  params: Promise<{ slug: string[] }>
+}): Promise<Metadata | undefined> {
+  const params = await props.params
+  const slug = decodeURI(params.slug.join('/'))
+  const post = allBlogs.find((p) => p.slug === slug)
+  if (!post) {
+    return
+  }
+
+  const publishedAt = new Date(post.date).toISOString()
+  const modifiedAt = new Date(post.lastmod || post.date).toISOString()
+  let imageList = [siteMeta.socialBanner]
+  if (post.images) {
+    imageList = typeof post.images === 'string' ? [post.images] : post.images
+  }
+
+  const ogImages = imageList.map((img) => {
+    return {
+      url: img.includes('http') ? img : siteMeta.siteUrl + img
+    }
+  })
+
+  return {
+    title: post.title,
+    description: post.summary,
+    openGraph: {
+      title: post.title,
+      description: post.summary,
+      siteName: siteMeta.title,
+      locale: 'en_US',
+      type: 'article',
+      publishedTime: publishedAt,
+      modifiedTime: modifiedAt,
+      url: './',
+      images: ogImages
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.summary,
+      images: imageList
+    }
+  }
+}
 
 export default function Page({ params }: { params: { slug: string[] } }) {
   const slug = decodeURI(params.slug.join('/'))
