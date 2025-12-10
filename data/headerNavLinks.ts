58b37872856658fb9314115877a575ac0822b809
“Austin” <30918019+haywang@users.noreply.github.com>
2025-12-10 10:51:57 +0800

    feat: Update tag data with new tags and add InitBrand link to header navigation.

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   // { href: '/tools', title: 'Tools' },
-  // { href: '/learning', title: 'Learning' },
+  { href: 'https://initbrand.dev', title: 'InitBrand' },
   { href: 'https://portfolio.austinfe.com', title: 'Portfolio' },
   { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' }
